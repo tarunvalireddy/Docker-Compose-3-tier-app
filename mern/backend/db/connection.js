6fb0d5debf9ffeba19a47c8f96abe9568a16a2fe
tarunvalireddy <tarunvalireddyy.com>
2025-07-10 09:46:27 +0530

    files-update

--- mern/backend/db/connection.js
+++ mern/backend/db/connection.js
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const URI = "mongodb://mongodb:27017";
+const URI = "mongodb://mongodb:27017;
+
 const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -10,15 +11,14 @@ const client = new MongoClient(URI, {
 });
 
 try {
-  // Connect the client to the server
   await client.connect();
-  // Send a ping to confirm a successful connection
   await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  console.log("✅ Connected to MongoDB!");
 } catch (err) {
-  console.error(err);
+  console.error("❌ MongoDB connection failed:", err);
 }
 
 let db = client.db("employees");
 
 export default db;
+
